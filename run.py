ca5d684e817a6774e3a99d5bc707a02a129c4dce
Thierry Schellenbach <thierryschellenbach@gmail.com>
2016-02-19 14:28:24 -0700

    cleanup

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from stream_framework.activity import Activity
 from benchmark import tasks
 import logging
+from stream_framework.verbs.base import *
 from stream_framework.verbs import get_verb_storage
 import time
 import click
@@ -42,6 +43,46 @@ LOGGING_DICT = {
 }
 dictConfig(LOGGING_DICT)
 
+@click.command()
+@click.option('--start-users', default=10, help='Number of greetings.')
+@click.option('--max-users', default=1000, help='Number of greetings.')
+@click.option('--multiplier', default=2, help='Number of greetings.')
+@click.option('--duration', default=3, help='Approximately the number of seconds to wait at every size of the userbase')
+def run_benchmark(start_users, max_users, multiplier, duration):
+    logger.info('Starting the benchmark! Exciting.... :)')
+    logger.info('Running with settings %s', locals())
+    logger.info('Synced the cassandra schema, all good')
+    
+    social_model = SocialModel(users=start_users)
+    days = 0
+    while True:
+        logger.info('Simulating a social network with %s users', social_model.users)
+        object_id = 1
+        for x in range(duration):
+            days += 1
+            logger.debug('Day %s for our network', days)
+            # create load based on the current model
+            active_users = social_model.active_users
+            for user_id in active_users:
+                # follow other users, note that we don't actually store the follower
+                #  lists for this benchmark
+                for x in range(2):
+                    tasks.follow_user(social_model, user_id, object_id % user_id)
+                # create activities
+                for x in range(social_model.get_user_activity(user_id)):
+                    activity = create_activity(user_id, object_id)
+                    object_id += 1
+                    tasks.add_user_activity.delay(social_model, user_id, activity)
+                # read a few pages of data
+                tasks.read_feed_pages(social_model, user_id)
+            time.sleep(1)
+                
+        # grow the network
+        logger.info('Growing the social network.....')
+        social_model.users = social_model.users * multiplier
+        if social_model.users > max_users:
+            logger.info('Reached the max users, we\'re done with our benchmark!')
+
 
 
 def create_activity(user_id, object_id):
@@ -75,7 +116,7 @@ class SocialModel(object):
     
     @property
     def active_users(self):
-        active_users = [u for u in range(1, self.users) if u % 100 < self.active_users_percentage]
+        active_users = [u for u in range(1, self.users) if u % 100 < self.daily_active_users_percentage]
         return active_users
 
     def get_browse_depth(self, user_id):
@@ -101,53 +142,6 @@ class SocialModel(object):
 
 
 
-
-        
-        
-
-        
-
-
-@click.command()
-@click.option('--start-users', default=10, help='Number of greetings.')
-@click.option('--max-users', default=1000, help='Number of greetings.')
-@click.option('--multiplier', default=2, help='Number of greetings.')
-@click.option('--duration', default=3, help='Approximately the number of seconds to wait at every size of the userbase')
-def run_benchmark(start_users, max_users, multiplier, duration):
-    logger.info('Starting the benchmark! Exciting.... :)')
-    logger.info('Running with settings %s', locals())
-    sync_cassandra()
-    logger.info('Synced the cassandra schema, all good')
-    
-    social_model = SocialModel(users=start_users)
-    days = 0
-    while True:
-        logger.info('Simulating a social network with %s users', social_model.users)
-        object_id = 1
-        for x in range(duration):
-            days += 1
-            logger.debug('Day %s for our network', days)
-            # create load based on the current model
-            active_users = social_model.active_users
-            for user_id in active_users:
-                # follow other users, note that we don't actually store the follower
-                #  lists for this benchmark
-                for x in range(2):
-                    tasks.follow_user(social_model, user_id, object_id % user_id)
-                # create activities
-                for x in range(social_model.get_user_activity(user_id)):
-                    activity = create_activity(user_id, object_id)
-                    object_id += 1
-                    tasks.add_user_activity.delay(social_model, user_id, activity)
-                # read a few pages of data
-                tasks.read_feed_pages(social_model, user_id)
-            time.sleep(1)
-                
-        # grow the network
-        logger.info('Growing the social network.....')
-        social_model.users = social_model.users * multiplier
-        if social_model.users > max_users:
-            logger.info('Reached the max users, we\'re done with our benchmark!')
         
         
 if __name__ == '__main__':

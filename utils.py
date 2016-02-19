fe1227f6e9acadcb2ce5e952bf3c81cedee943f7
kevvzub <kevin@getstream.io>
2016-02-19 15:54:27 -0700

    Added functions for number of friends, number of activities, number of pages with pseudo random proportions

--- utils.py
+++ utils.py
@@ -1,40 +1,53 @@
 from math import atan, pi
 
 
-def number_of_friends(active, network_size, scaling=1):
-    """0:non_active, 1:active, 2:celebrity"""
+def number_of_pages(id, network_size):
 
-    if active == 0:
+    if network_size < 1000:
+        return id % 2
+    else:
+        bin_number = (id/network_size)*1000
+        if 995 <= bin_number:
+            return 50
+        if 985 <= bin_number < 995:
+            return 20
+        if 965 <= bin_number < 985:
+            return 10
+        if 935 <= bin_number < 965:
+            return 5
+        if 735 <= bin_number < 935:
+            return 1
+        else:
+            return 0
+
+
+def number_of_friends(id, network_size, scaling=1):
+
+    bin_number = (id/network_size)*100
+
+    if bin_number <= 90:
         #No growth
         return 2
-    if active == 1:
+    if 89 < bin_number <= 99:
         avg_friends = 338 #steady state for active users
         #S-shaped growth on network size
         return avg_friends*(atan((network_size-250000000*scaling)/150000000*scaling) + pi/2)/pi
-    if active == 2:
+    else:
         #Linear growth on network size
-        #Using katy perry's twitter info, 25% of all active users follow her
         proportion_active_users = .17
         return .25*network_size*proportion_active_users
 
 
-def number_of_activities(active):
+def number_of_activities(id, network_size):
 
-    if active == 0:
+    bin_number = (id/network_size)*100
+
+    if bin_number <= 89:
         return 0
-    if active == 1:
+    if 89 < bin_number <= 98:
         return 1
-    if active == 2:
+    else:
         return 2.5 #super rough estimate using Obama's twitter account
 
 
-def number_of_pages(active, network_size, scaling=1):
 
-    if active == 0:
-        return 0
-    if active == 1:
-        avg_pages = 15 #steady state for active users
-        #S-shaped growth on network size
-        return avg_pages*(atan((network_size-250000000*scaling)/150000000*scaling) + pi/2)/pi
-    if active == 2:
-        return 5 #celebrities are too busy to spend all day on twitter?

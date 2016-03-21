77ac03544f85e95603507e1dc0cec2189e0d5a03
alessandro <alessandro@getstream.io>
2016-03-21 19:34:28 +0100

    Add python script for IPs discovery

--- get_ip.py
+++ get_ip.py
@@ -11,7 +11,9 @@ import sys
 #
 # Get the profile
 #
-def connect(region):
+def connect():
+    metadata = boto.utils.get_instance_metadata()
+    region = metadata['placement']['availability-zone'][:-1]
     profile = metadata['iam']['info']['InstanceProfileArn']
     profile = profile[profile.find('/') + 1:]
 
@@ -28,8 +30,8 @@ def connect(region):
 #
 # Print out private IPv4
 #
-def print_ips(region, tag_name):
-    conn = connect(region)
+def print_ips(tag_name):
+    conn = connect()
     reservations = conn.get_all_instances(filters={"tag:Name": tag_name})
 
     print("%s" % (reservations[0]["Instances"][0]["PrivateIpAddress"]))
@@ -40,10 +42,10 @@ def print_ips(region, tag_name):
 #
 opts, args = getopt.getopt(sys.argv[1:], "Lt:r:", ["tag-name", "region"])
 
+tag_name = ""
+region = ""
 for opt, arg in opts:
     if opt in ("-t", "--tag-name"):
         tag_name = arg
-    elif opt in ("-r", "--region"):
-        region = arg
 
-print_ips(region, tag_name)
+print_ips(tag_name)
